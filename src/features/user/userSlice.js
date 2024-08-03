463e12e0d00a9551a8ea91d1b0e4409129fdb116
Amit Tambulkar <amittambulkar104@gmail.com>
2024-08-03 06:39:09 +0530

    chore: dispatch and render username in the app

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -23,7 +23,7 @@ async function fetchAddress() {
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  username: "Amit",
+  username: "",
 };
 
 const userSlice = createSlice({
